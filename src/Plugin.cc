28a70f85850e7fd11cce0cceda049443666fb6b9
Christian Kreibich <christian@corelight.com>
2021-01-30 10:21:07 -0800

    Modernization for Zeek 4.1's removed deprecations
    This adds a bunch of zeek::detail namespacing, adoption of intrusive
    pointers, support for namespaced (Zeek 4.0+) vs unnamespaced (Zeek
    3.2) digest functions, and removes old checks for digestion code
    version that only applied to Bro era. The master branch now works with
    Zeek 3.2 up to master.
    
    Also bumps to version 1.5 and requires Zeek 3.2+ in zkg.meta.

--- src/Plugin.cc
+++ src/Plugin.cc
@@ -5,12 +5,12 @@ namespace plugin { namespace Corelight_CommunityID { Plugin plugin; } }
 
 using namespace plugin::Corelight_CommunityID;
 
-plugin::Configuration Plugin::Configure()
+zeek::plugin::Configuration Plugin::Configure()
 	{
-	plugin::Configuration config;
+	zeek::plugin::Configuration config;
 	config.name = "Corelight::CommunityID";
 	config.description = "\"Community ID\" flow hash support in the connection log";
 	config.version.major = 1;
-	config.version.minor = 1;
+	config.version.minor = 5;
 	return config;
 	}
