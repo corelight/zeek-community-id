596f78a4f24af4090fff69f2f992d1611558377b
Christian Kreibich <christian@corelight.com>
2023-01-11 18:18:29 -0800

    Switch use of bro_uint_t to zeek_uint_t
    This keeps a compatibility wrapper to work with Zeek 5.0.x and earlier.

--- src/zeek-compat.h
+++ src/zeek-compat.h
@@ -1,15 +1,22 @@
 #pragma once
 
-// Modest compatibility wrappers for differences between the Zeek 3.2
-// series and 4.0+. If things become too cumbersome to handle via this
-// header, we fork a dedicated zeek/x.y branch.
+// Modest compatibility wrappers for differences between Zeek versions.
+// If things become too cumbersome to handle via this header, we will
+// fork a dedicated zeek/x.y branch.
 
 // We drive the compatibilities purely from Zeek version numbers
 // (available since Zeek 3.1) since it saves us from implementing
 // feature checks.
 
-// Namespacing tweaks
 #include <zeek/zeek-config.h>
+
+// bro_uint_t vs zeek_uint_t: we have the latter as of 5.1, and lose the former
+// in 6.1.
+#if ZEEK_VERSION_NUMBER < 50100
+#define zeek_uint_t bro_uint_t
+#endif
+
+// Namespacing tweaks
 #if ZEEK_VERSION_NUMBER >= 30200 && ZEEK_VERSION_NUMBER < 40000
 #define ZEEK_DETAIL_NS
 #elif ZEEK_VERSION_NUMBER >= 40000
